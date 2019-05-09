2917d5de6827fcc2542858097f276b8025800147
Märt Bakhoff <mart.bakhoff@roguewave.com>
2019-05-09 19:11:06 +0300

    fixpack3

--- src/main/java/com/zeroturnaround/cloudmonitor/CloudNodeListener.java
+++ src/main/java/com/zeroturnaround/cloudmonitor/CloudNodeListener.java
@@ -4,7 +4,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -25,37 +24,32 @@ public class CloudNodeListener extends ComputerListener {
 
   @Override
   public void onOnline(Computer c, TaskListener listener) {
-    try {
-      logStatus(c, "online");
-    }
-    catch (Exception e) {
-      log.log(Level.SEVERE, "logging node status" + c.getName(), e);
-    }
+    logStatus(c, "online");
   }
 
   @Override
   public void onOffline(Computer c, OfflineCause cause) {
-    try {
-      logStatus(c, "offline");
-    }
-    catch (IOException e) {
-      log.log(Level.SEVERE, "logging node status" + c.getName(), e);
-    }
+    logStatus(c, "offline");
   }
 
-  private void logStatus(Computer c, String status) throws IOException {
-    Path logFile = StorageConfig.getLogFile("node-status.log");
-    if (logFile == null)
-      return;
-
+  private void logStatus(Computer c, String status) {
     String name = c.getName().trim();
     if (name.isEmpty())
       name = c.getClass().getSimpleName();
 
-    String timestamp = String.valueOf(System.currentTimeMillis());
-    byte[] logLine = (encodeStatus(name, timestamp, status) + "\n").getBytes(UTF_8);
-    synchronized (this) {
-      Files.write(logFile, logLine, CREATE, APPEND);
+    try {
+      Path logFile = StorageConfig.getLogFile("node-status.log");
+      if (logFile == null)
+        return;
+
+      String timestamp = String.valueOf(System.currentTimeMillis());
+      byte[] logLine = (encodeStatus(name, timestamp, status) + "\n").getBytes(UTF_8);
+      synchronized (this) {
+        Files.write(logFile, logLine, CREATE, APPEND);
+      }
+    }
+    catch (Exception e) {
+      log.log(Level.SEVERE, "logging node status: " + name, e);
     }
   }
 
